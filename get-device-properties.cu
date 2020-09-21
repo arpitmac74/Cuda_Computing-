1c1f13c300887bbbb46ce81c132b5a4f1bb920fb
arpitmac74 <arpitmac74@gmail.com>
2020-09-21 15:34:39 -0400

    a

---
+++ get-device-properties.cu
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+  /*
+   * Assign values to these variables so that the output string below prints the
+   * requested properties of the currently active GPU.
+   */
+
+  int deviceId;
+  cudaGetDevice(&deviceId);
+  
+  cudaDeviceProp props;
+  cudaGetDeviceProperties(&props, deviceId);
+  
+  
+  int computeCapabilityMajor = props.major;
+  int computeCapabilityMinor = props.minor;
+  
+  int multiProcessorCount = props.multiProcessorCount;
+  int warpSize = props.warpSize;
+  char* names = props.name;
+  
+
+  /*
+   * There should be no need to modify the output string below.
+   */
+
+  printf("Device ID: %d\nNumber of SMs: %d\nCompute Capability Major: %d\nCompute Capability Minor: %d\nWarp Size: %d\nName: %s\n", deviceId, multiProcessorCount, computeCapabilityMajor, computeCapabilityMinor, warpSize,names);
+}
+
